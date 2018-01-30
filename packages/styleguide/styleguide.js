89529b5b789cd526f33cdccb153292b1fdeadd77
Gaetano Checinski <gaetano.checinski@gmail.com>
2018-01-30 16:54:30 +0000

    implements styleguide colors in storybook

---
+++ packages/styleguide/styleguide.js
@@ -0,0 +1,72 @@
+export const WeekendColours = {
+  money: "#006A74",
+  newsReview: "#004D6D",
+  travel: "#2C79AD",
+  home: "#8D724D",
+  irishSport: "#01000D",
+  sundayTimesMagazine: "#1D1D1B",
+  style: "#BC3385",
+  culture: "#7B0046",
+  saturdayReview: "#A31D24",
+  weekend: "#05829A",
+  theTimesMagazine: "#1D1D1D"
+};
+
+export const WeekdaysColours = {
+  default: "#1D1D1B",
+  news: "#13354E",
+  comment: "#850029",
+  world: "#636C17",
+  business: "#005B8D",
+  law: "#691D26",
+  register: "#6C6C69",
+  sport: "#008347",
+  ireland: "#004E45",
+  scotland: "#00233E",
+  puzzles: "#C74600",
+  leaders: "#F4f2e8",
+  defcon: "#FF1d25",
+  focus: "#CC0000",
+  theGame: "#006469",
+  theDish: "#DB133B",
+  bricksMortar: "#006469",
+  techGames: "#1A1f24",
+  gardening: "#00313B",
+  driving: "#C04605",
+  times2: "#622956",
+  artsFeatures: "#622956",
+  todaysMagazines: "#1D1D1B",
+  timesMagazine: "#1D1D1B",
+  styleMagazine: "#BC3385",
+  cultureMagazine: "#7B0046"
+};
+
+export const UIColours = {
+  timesBlack: "#1D1D1B",
+  midGrey: "#696969",
+  darkGrey: "#333333",
+  keylineGrey: "#DBDBDB",
+  linkBlue: "#006699",
+  notificationBlue: "#3C81BE",
+  backgroundGrey: "#F9F9F9",
+  backgroundBuff: "#F9F8F3",
+  alertYellow: "#FCC435"
+};
+
+export const Colours = {
+  ...WeekendColours,
+  ...WeekdaysColours,
+  ...UIColours
+};
+
+// TODO: find usage patterns and propper names
+export const Fonts = {
+  GillSans: "GillSansMTStd-Medium",
+  timesDigitalRegularSc: "TimesDigitalW04-RegularSC",
+  timesDigitalRegular: "TimesDigitalW04-Regular",
+  timesDigital: "TimesDigitalW04",
+  TimesModernBold: "TimesModern-Bold",
+  timesModern: "TimesModern-Regular"
+};
+
+export const Spacing = {};
---
+++ packages/styleguide/styleguide.stories.js
@@ -0,0 +1,106 @@
+import "react-native";
+import React from "react";
+import { Text, View } from "react-native";
+import { storiesOf } from "dextrose/storiesOfOverloader";
+import {
+  UIColours,
+  WeekdaysColours,
+  WeekendColours,
+  Fonts
+} from "./styleguide";
+
+const styles = {
+  colorContainer: {
+    flexDirection: "column",
+    alignItems: "center"
+  },
+
+  colourBox: {
+    flexWrap: "wrap",
+    flexDirection: "row"
+  },
+
+  label: {
+    padding: 5,
+    paddingLeft: 10,
+    paddingRight: 10
+  },
+
+  box: {
+    width: 100,
+    height: 80,
+    margin: 25,
+    padding: 5,
+    borderRadius: 5
+  },
+
+  fontContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 20
+  },
+
+  text: {
+    borderColor: "black",
+    width: 250,
+    fontSize: 20
+  }
+};
+
+const FontSwatch = props => (
+  <View
+    style={{
+      ...styles.fontContainer
+    }}
+  >
+    <Text
+      style={{
+        ...styles.text,
+        fontFamily: props.font
+      }}
+    >
+      {" "}
+      Lorem ipsum dolor sit amet{" "}
+    </Text>
+    <Text style={styles.label}>{props.label}</Text>
+    <Text style={styles.label}>{props.font}</Text>
+  </View>
+);
+
+const ColourSwatch = props => (
+  <View style={styles.colorContainer}>
+    <View style={[styles.box, { backgroundColor: props.colour }]} />
+    <Text style={styles.label}>{props.label}</Text>
+    <Text style={styles.label}>{props.colour}</Text>
+  </View>
+);
+
+storiesOf("Styleguide", module)
+  .add("UI Colours", () => (
+    <View style={styles.colourBox}>
+      {Object.entries(UIColours).map(([label, colour]) => (
+        <ColourSwatch label={label} colour={colour} key={label + colour} />
+      ))}
+    </View>
+  ))
+  .add("Weekdays Colours", () => (
+    <View style={styles.colourBox}>
+      {Object.entries(WeekdaysColours).map(([label, colour]) => (
+        <ColourSwatch label={label} colour={colour} key={label + colour} />
+      ))}
+    </View>
+  ))
+  .add("Weekend Colours", () => (
+    <View style={styles.colourBox}>
+      {Object.entries(WeekendColours).map(([label, colour]) => (
+        <ColourSwatch label={label} colour={colour} key={label + colour} />
+      ))}
+    </View>
+  ))
+  .add("Fonts", () => (
+    <View style={styles.fontBox}>
+      {Object.entries(Fonts).map(([label, font]) => (
+        <FontSwatch label={label} font={font} key={label + font} />
+      ))}
+    </View>
+  ));
